82fcb91071d425f5f8f82e1762f8f13025a41896
sevenChrisK <chris@plant-parenthood.co.uk>
2021-09-09 19:49:11 +0100

    game function impelmented, round win counter in progress

--- rps-script.js
+++ rps-script.js
@@ -3,21 +3,20 @@
 // create function for computerPlay
 function computerPlay () {
   // generate a random number between 0 and 1
-  let randomNum = Math.random()
+  let randomNum = Math.floor(Math.random() * 100)
 
-  // declare variable to hold computer choice
   let computerChoice
 
   // assign a choice based on value of random number
-  if (randomNum <= 0.333) {
-    let computerChoice = 'Rock'
-  } else if (randomNum >= 0.334 && randomNum < 0.666) {
-    let computerChoice = 'Paper'
-  } else if (randomNum >= 0.666) {
-    let computerChoice = 'Sissors'
+  if (randomNum <= 33) {
+    computerChoice = 'Rock'
+  } else if (randomNum >= 34 && randomNum < 66) {
+    computerChoice = 'Paper'
+  } else if (randomNum >= 66) {
+    computerChoice = 'Sissors'
   }
 
-  // return the result
+  console.log('The computer chose: ' + computerChoice)
   return computerChoice
 }
 
@@ -26,8 +25,6 @@ function capitalizeFirstLetter (string) {
 }
 
 function gameRound () {
-  let roundResult
-
   // create variable to hold player choice, get input via prompt, convert to lowercase
   let playerInput = prompt('Rock, Paper or Sissors?')
   let playerLowerCase = playerInput.toLowerCase()
@@ -37,23 +34,62 @@ function gameRound () {
   let computerChoice = computerPlay()
 
   // logic to determine winner of round
+  let roundResult
+  let roundWinner
   if (
-    ((computerChoice == 'Rock') && (playerChoice == 'Paper')) ||
-    ((computerChoice == 'Paper') && (playerChoice == 'Sissors')) ||
-    ((computerChoice == 'Sissors') && (playerChoice == 'Rock'))
+    (computerChoice == 'Rock' && playerChoice == 'Paper') ||
+    (computerChoice == 'Paper' && playerChoice == 'Sissors') ||
+    (computerChoice == 'Sissors' && playerChoice == 'Rock')
   ) {
-    let roundResult = console.log(
+    roundResult = console.log(
       `${playerChoice} beats ${computerChoice}, you win this round!`
     )
+    roundWinner = 'Player'
+    return roundWinner
   } else if (
-    ((computerChoice == 'Rock') && (playerChoice == 'Sissors')) ||
-    ((computerChoice == 'Paper') && (playerChoice == 'Rock')) ||
-    ((computerChoice == 'Sissors') && (playerChoice == 'Paper'))
+    (computerChoice == 'Rock' && playerChoice == 'Sissors') ||
+    (computerChoice == 'Paper' && playerChoice == 'Rock') ||
+    (computerChoice == 'Sissors' && playerChoice == 'Paper')
   ) {
-    let roundResult = console.log(
+    roundResult = console.log(
       `${computerChoice} beats ${playerChoice}, you lose this round!`
     )
+    roundWinner = 'Computer'
+    return roundWinner
+  } else if (computerChoice == playerChoice) {
+    roundResult = console.log(
+      `${playerChoice}...ha! you read my mind! this round is a draw!`
+    )
+    roundWinner = 'Draw'
+    return roundWinner
   }
+  return roundWinner
+}
 
-  return roundResult 
+function game () {
+  let computerWins = 0
+  let playerWins = 0
+  let drawnRounds = 0
+
+  for (let i = 0; i < 5; i++) {
+    let roundWinner = gameRound()
+    if ((roundWinner == 'Computer')) {
+      computerWins++
+    } else if ((roundWinner == 'Player')) {
+      playerWins++
+    } else if ((roundWinner == 'Draw')) {
+      drawnRounds++
+    }
+  }
+  if (computerWins > playerWins) {
+    console.log(
+      `Computer won ${computerWins} rounds, you won ${playerWins} (${drawnRounds} drawn rounds) - better luck next time!`
+    )
+  } else if (playerWins > computerWins) {
+    console.log(
+      `Computer won ${computerWins} rounds, yet you won ${playerWins} (${drawnRounds} drawn rounds) - CONGRATULATIONS!`
+    )
+  } else if (playerWins == computerWins) {
+    console.log('All draws! How irregular!')
+  }
 }
